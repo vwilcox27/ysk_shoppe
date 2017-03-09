d303615b42ab461f152d7b02b0b19f40fcfa82b0
Peter Wilcox <peter.wilcox@echostar.com>
2017-03-08 19:21:55 -0700

    Updated

--- app/controllers/lookbook_controller.rb
+++ app/controllers/lookbook_controller.rb
@@ -1,2 +1,9 @@
 class LookbookController < ApplicationController
+  def lookbook
+    @lookbook = Shoppe::Lookbook.root.ordered.includes(:product_categories, :variants)
+    @lookbook = @lookbook.group_by(&:lookbook_category)
+  end
+  def show
+    # @contact = Shoppe::Contact.root.find_by_permalink(params[:permalink])
+  end
 end
