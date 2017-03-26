3f3312aacbfbe2e06de2493a398c3567e5b8eec9
Peter Wilcox <peter.wilcox@echostar.com>
2017-03-26 00:52:59 -0600

    More styling.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require bootstrap
+//= require bootstrap.min
 //= require_tree .
 //= require shoppe/stripe/form_handler
